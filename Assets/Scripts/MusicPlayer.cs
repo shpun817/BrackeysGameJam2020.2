80dc8cebb3e67f62613915c4c8c91d6c498966f7
shpun817 <shpun@connect.ust.hk>
2020-08-06 23:25:25 +0800

    Added Music Player object (persistent)

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake() {
+		if (GameObject.FindGameObjectsWithTag("Music").Length > 1) {
+			Destroy(gameObject);
+		} else {
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+}
